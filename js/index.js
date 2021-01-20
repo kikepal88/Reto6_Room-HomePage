a2536e7cbb7b07d514b2a3a1acbeab6ba59d4526
Omar Palacios <omarpal88@gmail.com>
2021-01-19 20:20:05 -0500

    Header terminado con estilos y JS

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function showNav1 () {
+  document.getElementById("burger-nav").classList.toggle("close-icon");
+  document.getElementById("header").classList.toggle("is-active");
+  document.getElementById("nav-bar").classList.toggle("is-active");
+};
